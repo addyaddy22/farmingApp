5765234dd818419e81ec838da6cc89edacbe7d9b
addyaddy22 <mutamboadmire2@gmail.com>
2025-03-27 02:49:02 +0200

    requirements txt file added using the pip command

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from app.controllers.user_controller import router as user_router
 from app.controllers.data_controller import router as data_router
@@ -7,3 +8,8 @@ app = FastAPI()
 # Include routers
 app.include_router(user_router, prefix="/api/users")
 app.include_router(data_router, prefix="/api/data")
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", "8001"))  # Default to 8001 if not set
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
