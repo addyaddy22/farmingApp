549e8d1d83acdd4c7c19b7c8ef1ef1ee5f0dc722
addyaddy22 <mutamboadmire2@gmail.com>
2025-03-27 02:31:26 +0200

    changes to the sql models

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 from app.database import engine, Base
-from app.models.models import User, FarmData
+from app.models.models import User, FarmData,FarmDataOptions
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
